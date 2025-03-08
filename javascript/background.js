6b93f9c040373cd6245b0bc41e28980b16a4b4ef
Adrian Villanueva Echepari <florie.echepari@gmail.com>
2025-03-08 12:20:45 +0800

    Added JS Background

---
+++ javascript/background.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    VANTA.FOG({
+        el: "#background",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        highlightColor: 0x460000,
+        midtoneColor: 0x0,
+        lowlightColor: 0x0,
+        baseColor: 0x2a2525,
+        blurFactor: 0.29,
+        speed: 0.00
+    });
+});
